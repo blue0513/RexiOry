ea9f7d23b8298cd4ec26927a207b73bc8c2d0ac5
Taiju Aoki <aokitaiju0513@gmail.com>
2023-05-07 14:00:58 +0900

    Sort by most recent viewed (#4)

--- src/const.js
+++ src/const.js
@@ -2,6 +2,7 @@ export const MAX_HISTORY_RETRIEVE_COUNT = 10000;
 export const MAX_HISTORY_VIEW_COUNT = 100;
 export const SEARCH_DEBOUNCE = 100;
 export const MOVE_DEBOUNCE = 10;
+export const SORT_BY_SCORE = false;
 export const SEARCH_FORM_CLASS = "search";
 export const HISTORY_LIST_CLASS = "historyItemList";
 export const HISTORY_ITEM_CLASS = "historyItem";
