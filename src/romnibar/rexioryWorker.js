46ed986320a9dfb0a878cebd355c28d37bc96a8b
Taiju Aoki <aokitaiju0513@gmail.com>
2024-01-10 13:35:18 +0900

    fix injection css timing

--- src/romnibar/rexioryWorker.js
+++ src/romnibar/rexioryWorker.js
@@ -1,25 +1,26 @@
-chrome.webNavigation.onDOMContentLoaded.addListener(async ({ tabId }) => {
-  const injectionCSS = {
-    target: { tabId: tabId },
-    files: ["css/style.css"],
-  };
-  chrome.scripting.insertCSS(injectionCSS);
-});
-
 chrome.commands.onCommand.addListener(async (command) => {
   switch (command) {
     case "show": {
-      await sendShowCommand();
+      const tab = await activeTab();
+      if (!tab) return;
+
+      await injectCSS(tab.id);
+      await sendShowCommand(tab.id);
       break;
     }
   }
 });
 
-async function sendShowCommand() {
-  const tab = await activeTab();
-  if (!tab) return;
+async function injectCSS(tabId) {
+  const injectionCSS = {
+    target: { tabId: tabId },
+    files: ["css/style.css"],
+  };
+  await chrome.scripting.insertCSS(injectionCSS);
+}
 
-  await chrome.tabs.sendMessage(tab.id, { command: "show", tabId: tab.id });
+async function sendShowCommand(tabId) {
+  await chrome.tabs.sendMessage(tabId, { command: "show", tabId: tabId });
 }
 
 async function activeTab() {
